cceeb1f63c063b6597d97e5cc3da1aa8a67f0323
Angelo Hizon <angelo.hizon27@gmail.com>
2021-07-20 23:22:56 +0800

    another changes for favicon

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const https = require("https");
 // external module required for favicon.ico
-// const favicon = require('serve-favicon')
+const favicon = require('serve-favicon')
 const path = require('path')
 
 const app = express();
@@ -10,7 +10,7 @@ const app = express();
 // for some reason this allows the use of CSS
 app.use(express.static(__dirname));
 // takes the favicon from the directory
-// app.use(favicon(__dirname + "/images/favicon/favicon.ico"));
+app.use(favicon(__dirname + "/images/favicon/favicon.ico"));
 app.use(bodyParser.urlencoded( {extended:true} ));
 
 app.listen(process.env.PORT || 3000, function() {
